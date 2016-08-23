949af019351bc7a0154b34188e0bb990f5bfa0e1
ehsan <mousavimadani.e@gmail.com>
2016-08-23 20:55:19 +0430

    footer ( sign & gallery without js)

--- resources/views/landingViews/gallery.blade.php
+++ resources/views/landingViews/gallery.blade.php
@@ -1,7 +1,119 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: amirhosein
- * Date: 8/18/2016 AD
- * Time: 12:56
- */
+<html>
+
+<head>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta charset="UTF-8">
+    <title>Gallery</title>
+    <link rel="stylesheet" href="./source/galstyle.css">
+    <script src="./source/js/jquery-3.1.0.js"></script>
+</head>
+<body>
+<div class="gallery">
+<div class="container-full container center-block centered">
+    <div class ="gal_row">
+        <div class="col" >
+            <img src="./uploads/gal_1.jpg" alt="pic1" height="235" width="275" id="image1"/>
+        </div>
+        <div class="col">
+            <img src="./uploads/gal_2.jpg" alt="pic2" height="235" width="275" id="image2"/>
+        </div>
+        <div class="col">
+            <img src="./uploads/gal_3.jpg" alt="pic3" height="235" width="275" id="image3"/>
+        </div>
+        <div class="col">
+            <img src="./uploads/gal_4.jpg" alt="pic4" height="235" width="275" id="image4"/>
+        </div>
+    </div>
+</div>
+
+
+<i class="fa fa-anchor">
+
+</i>"
+
+<br>
+    <div class ="gal_row text-center">
+
+        <input class="btn btn-default" value="PREVIOUS" id="prev" name="prev" onclick="prev();" >
+        <input class="btn btn-default" value="NEXT" id="next" name="next" onclick="next();" >
+
+        {{--<i class="fa fa-arrow-right" id="prev" name="prev" onclick="prev();">--}}
+
+        {{--</i>--}}
+
+        {{--<i class="fa fa-arrow-left" id="next" name="next" onclick="next();">--}}
+
+        {{--</i>--}}
+
+        {{--<input type="button"  class="btn btn-default fa fa-arrow-left" value="NEXT" id="next" name="next" onclick="next();" >--}}
+    </div>
+</div>
+
+<script >
+    var images1 = [];
+    images.push("../../uploads/gal_1.jpg");
+    images.push("../../uploads/gal_2.jpg");
+    images.push("../../uploads/gal_3.jpg");
+    images.push("../../uploads/gal_4.jpg");
+
+    var images2 = [];
+    images.push("../../uploads/gal_2.jpg");
+    images.push("../../uploads/gal_3.jpg");
+    images.push("../../uploads/gal_4.jpg");
+    images.push("../../uploads/gal_1.jpg");
+
+    var images3 = [];
+    images.push("../../uploads/gal_3.jpg");
+    images.push("../../uploads/gal_4.jpg");
+    images.push("../../uploads/gal_1.jpg");
+    images.push("../../uploads/gal_2.jpg");
+
+    var images4 = [];
+    images.push("../../uploads/gal_4.jpg");
+    images.push("../../uploads/gal_1.jpg");
+    images.push("../../uploads/gal_2.jpg");
+    images.push("../../uploads/gal_3.jpg");
+
+    var num = 3;
+    var cur = 1;
+
+    var im1=$("#image1");
+    var im2=$("#image2");
+    var im3=$("#image3");
+    var im4=$("#image4");
+
+    function prev(){
+        if(cur>0)
+        {
+
+            im1.attr('src', images1[cur-1]);
+            im2.attr('src', images2[cur-1]);
+            im3.attr('src', images3[cur-1]);
+            im4.attr('src', images4[cur-1]);
+            cur = cur - 1;
+
+        }
+
+    }
+
+    function next(){
+
+        if(cur < num){
+
+            im1.attr('src', images1[cur+1]);
+            im2.attr('src', images2[cur+1]);
+            im3.attr('src', images3[cur+1]);
+            im4.attr('src', images4[cur+1]);
+            cur = cur + 1;
+
+        }
+
+    }
+
+
+</script>
+
+</body>
+
+</html>
--- resources/views/landingViews/sign.blade.php
+++ resources/views/landingViews/sign.blade.php
@@ -1,7 +1,99 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: amirhosein
- * Date: 8/18/2016 AD
- * Time: 12:57
- */
+<div class="sign">
+    <div class="container-full centered ">
+        <div class="row center-block centered text-center">
+            <div class="col" >
+                <img class="logo img-responsive center-block" src="uploads/logo.png">
+            </div>
+        </div>
+        <p class ="row text text-center">
+            عضویت در خبرنامه
+        </p>
+        <form class="form-horizontal">
+            <div class="row">
+            <div class="form-group center-block">
+                <div class="col">
+                    <input type="email" class="form-control" id="email" placeholder="Email*">
+                </div>
+            </div>
+            </div>
+            <div class="row">
+            <div class="form-group center-block">
+                <div class="col">
+                    <button class="btn btn-default" type="submit" >عضو شوید</button>
+                </div>
+            </div>
+            </div>
+        </form>
+
+
+        <div class="row row_social">
+            <div class="col">
+                <a href="#">
+                    <img  alt="1" src="uploads/circle_white.png">
+                </a>
+            </div>
+            <div class="col">
+                <a href="#">
+                    <img  alt="1" src="uploads/circle_white.png">
+                </a>
+            </div>
+            <div class="col">
+                <a href="#">
+                    <img  alt="1" src="uploads/circle_white.png">
+                </a>
+            </div>
+            <div class="col">
+                <a href="#">
+                    <img  alt="1" src="uploads/circle_white.png">
+                </a>
+            </div>
+            <div class="col">
+                <a href="#">
+                    <img  alt="1" src="uploads/circle_white.png">
+                </a>
+            </div>
+            <div class="col">
+                <a href="#">
+                    <img  alt="1" src="uploads/circle_white.png">
+                </a>
+            </div>
+            <div class="col">
+                <a href="#">
+                    <img  alt="1" src="uploads/circle_white.png">
+                </a>
+            </div>
+            <div class="col">
+                <a href="#">
+                    <img  alt="1" src="uploads/circle_white.png">
+                </a>
+            </div>
+            <div class="col">
+                <a href="#">
+                    <img  alt="1" src="uploads/circle_white.png">
+                </a>
+            </div>
+        </div>
+
+        <hr width="750 px">
+        <div class="container container_links">
+            <div class="row row_links ">
+                <div class="col">
+                    تمامی حقوق مادی و معنوی وب سایت و اپلیکیشن متعلق به گروه آشپزباشی می باشد.
+                </div>
+            <div class="col">
+                <a class="link" href="#">درباره ما</a>
+                <a class="link" href="#">حریم خصوصی</a>
+                <a class="link" href="#">تماس با ما</a>
+            </div>
+
+            </div>
+        </div>
+</div>
+</div>
+
+
+
+
+
+
+
